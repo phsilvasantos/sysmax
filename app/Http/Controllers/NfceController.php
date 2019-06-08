22e87acf8803565ce09400db81281396c6430537
johng <johngna@gmail.com>
2019-06-08 11:37:35 -0300

    08062019

--- app/Http/Controllers/NfceController.php
+++ app/Http/Controllers/NfceController.php
@@ -881,7 +881,7 @@ class NfceController extends AppController
                 $consulta = self::consulta($venda, $empresa, $recibo);
 
 
-                return response()->file(url('storage/arquivos/empresa_id_'.'\\'.'xml\\'.$nfce[0]->mesAno .'\\PDFs\\'.$nfce[0]->arquivo.'.pdf'));    // __DIR__ .'\..\..\..\storage\app\public\arquivos\empresa_id_'. $empresa->id .'\\'.'xml\\'.$nfce[0]->mesAno .'\\PDFs\\'.$nfce[0]->arquivo.'.pdf');
+                return response()->file(url('storage/arquivos/empresa_id_'. $empresa->id .'\\'.'xml\\'.$nfce[0]->mesAno .'\\PDFs\\'.$nfce[0]->arquivo.'.pdf'));    // __DIR__ .'\..\..\..\storage\app\public\arquivos\empresa_id_'. $empresa->id .'\\'.'xml\\'.$nfce[0]->mesAno .'\\PDFs\\'.$nfce[0]->arquivo.'.pdf');
 
             }else{
 
